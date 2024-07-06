62bd494cf4b09af139644455581098d426fd348d
miavai649 <mahmudulhaquenoor@gmail.com>
2024-07-06 16:52:02 +0600

    some action ignored for serializablecheck avoid

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,7 +2,16 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './features/auth/authSlice'
 import { baseApi } from './api/baseApi'
 import storage from 'redux-persist/lib/storage'
-import { persistReducer, persistStore } from 'redux-persist'
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE
+} from 'redux-persist'
 
 const persistConfig = {
   key: 'auth',
@@ -17,7 +26,11 @@ export const store = configureStore({
     auth: persistedReducer
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(baseApi.middleware)
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    }).concat(baseApi.middleware)
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
---
+++ test.js
@@ -0,0 +1,10 @@
+const obj = {
+  name: 'noor',
+  hobby: 'gaming',
+  age: 22,
+  greet: () => {
+    return 'hello world'
+  }
+}
+
+console.log(JSON.stringify(obj))
