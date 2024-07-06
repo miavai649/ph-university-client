5e79cc73b65220840e7fae2d7ae227d2f2099bc4
miavai649 <mahmudulhaquenoor@gmail.com>
2024-07-07 01:06:53 +0600

    log out button added and also log out dispatch function added and also installed sonner

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,15 +1,25 @@
-import { Layout } from 'antd'
+import { Button, Layout } from 'antd'
 import { Outlet } from 'react-router-dom'
 import Sidebar from './Sidebar'
+import { useAppDispatch } from '../../redux/hooks'
+import { logout } from '../../redux/features/auth/authSlice'
 
 const { Header, Content } = Layout
 
 const MainLayout = () => {
+  const dispatch = useAppDispatch()
+
+  const handleLogOut = () => {
+    dispatch(logout())
+  }
+
   return (
     <Layout style={{ height: '100vh' }}>
       <Sidebar />
       <Layout>
-        <Header />
+        <Header>
+          <Button onClick={handleLogOut}>Log out</Button>
+        </Header>
         <Content style={{ margin: '24px 16px 0' }}>
           <div
             style={{
