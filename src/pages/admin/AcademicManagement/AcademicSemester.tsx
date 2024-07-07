ed0f5e738b3d4c5a7d1bdbd3cbe16cb61f4f2f7e
miavai649 <mahmudulhaquenoor@gmail.com>
2024-07-07 18:38:22 +0600

    getAllAcademic|Semester api created by rtk query

--- src/pages/admin/AcademicManagement/AcademicSemester.tsx
+++ src/pages/admin/AcademicManagement/AcademicSemester.tsx
@@ -1,4 +1,15 @@
+import { academicSemesterApi } from '../../../redux/features/academicSemester/academicSemesterApi'
+
 const AcademicSemester = () => {
+  const { data, isError, isLoading, isSuccess } =
+    academicSemesterApi.useGetAllAcademicSemesterQuery(undefined)
+  console.log('🚀 ~ AcademicSemester ~ data:', {
+    data,
+    isError,
+    isLoading,
+    isSuccess
+  })
+
   return (
     <div>
       <h1>This is AcademicSemester component</h1>
--- src/redux/features/academicSemester/academicSemesterApi.ts
+++ src/redux/features/academicSemester/academicSemesterApi.ts
@@ -2,7 +2,7 @@ import { baseApi } from '../../api/baseApi'
 
 export const academicSemesterApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    getAllAcademicSemester: builder.mutation({
+    getAllAcademicSemester: builder.query({
       query: () => ({
         url: '/academic-semesters',
         method: 'GET'
