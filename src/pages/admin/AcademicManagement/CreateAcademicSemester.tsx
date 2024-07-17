5e1671158b000ee16d425ef985793a1b049c8b93
miavai649 <mahmudulhaquenoor@gmail.com>
2024-07-18 01:41:17 +0600

    create academic semester page initialize and phform and ph input component modified

--- src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
@@ -1,8 +1,23 @@
+import { FieldValues, SubmitHandler } from 'react-hook-form'
+import PHForm from '../../../components/form/PHForm'
+import PHInput from '../../../components/form/PHInput'
+import { Button, Col, Flex } from 'antd'
+
 const CreateAcademicSemester = () => {
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data)
+  }
+
   return (
-    <div>
-      <h1>This is CreateAcademicSemester component</h1>
-    </div>
+    <Flex justify='center' align='center'>
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHInput type={'text'} name={'name'} label='Name' />
+          <PHInput type={'text'} name={'year'} label='Year' />
+          <Button htmlType='submit'>Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   )
 }
 
