d1ecb2594dd45581084e175dab1500b76ebbdb19
miavai649 <mahmudulhaquenoor@gmail.com>
2024-07-18 10:12:45 +0600

    semester name and code getting in onsubmit function

--- src/components/form/PHSelect.tsx
+++ src/components/form/PHSelect.tsx
@@ -13,7 +13,12 @@ const PHSelect = ({ label, name, options }: TSelectProps) => {
       name={name}
       render={({ field }) => (
         <Form.Item label={label}>
-          <Select style={{ width: '100%' }} {...field} options={options} />
+          <Select
+            style={{ width: '100%' }}
+            {...field}
+            options={options}
+            size='large'
+          />
         </Form.Item>
       )}
     />
