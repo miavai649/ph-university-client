1c23d2759d0ba8cabc26b5ed4882bba7c8716945
miavai649 <mahmudulhaquenoor@gmail.com>
2024-07-18 10:12:06 +0600

    PHSelect component modified

--- src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
@@ -4,18 +4,37 @@ import PHInput from '../../../components/form/PHInput'
 import { Button, Col, Flex } from 'antd'
 import PHSelect from '../../../components/form/PHSelect'
 
+const nameOptions = [
+  {
+    value: '01',
+    label: 'Autumn'
+  },
+  {
+    value: '02',
+    label: 'Summer'
+  },
+  {
+    value: '03',
+    label: 'Fall'
+  }
+]
+
 const CreateAcademicSemester = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    console.log(data)
+    const name = nameOptions[Number(data.name) - 1].label
+
+    const semesterData = {
+      name,
+      code: data.name
+    }
+    console.log(semesterData)
   }
 
   return (
     <Flex justify='center' align='center'>
       <Col span={6}>
         <PHForm onSubmit={onSubmit}>
-          <PHInput type={'text'} name={'name'} label='Name' />
-          <PHInput type={'text'} name={'year'} label='Year' />
-          <PHSelect label='name' />
+          <PHSelect label='name' name='name' options={nameOptions} />
           <Button htmlType='submit'>Submit</Button>
         </PHForm>
       </Col>
