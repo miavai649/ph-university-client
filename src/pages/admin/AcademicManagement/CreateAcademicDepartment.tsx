6314bce134361b1bd3157dcedfb82e033bbb83bf
miavai649 <mahmudulhaquenoor@gmail.com>
2024-08-20 20:16:20 +0600

    create academic department form created

--- src/pages/admin/AcademicManagement/CreateAcademicDepartment.tsx
+++ src/pages/admin/AcademicManagement/CreateAcademicDepartment.tsx
@@ -1,19 +1,36 @@
-import { Col, Flex } from 'antd'
+import { Button, Col, Flex } from 'antd'
 import PHForm from '../../../components/form/PHForm'
 import PHInput from '../../../components/form/PHInput'
 import { FieldValues, SubmitHandler } from 'react-hook-form'
+import { academicManagementApi } from '../../../redux/features/admin/academicManagement.api'
+import PHSelect from '../../../components/form/PHSelect'
 
 const CreateAcademicDepartment = () => {
+  const { data: facultyData } =
+    academicManagementApi.useGetAllAcademicFacultyQuery(undefined)
+
+  const facultyOptions =
+    facultyData?.data?.map((faculty) => ({
+      value: faculty?._id,
+      label: faculty?.name
+    })) || []
+
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     console.log(data)
   }
 
   return (
-    <Flex>
+    <Flex justify='center'>
       <Col span={6}>
         <h1 style={{ marginBottom: '1rem' }}>Create Academic Department</h1>
         <PHForm onSubmit={onSubmit}>
           <PHInput type='text' name='name' label='Name' />
+          <PHSelect
+            label='Academic Faculty'
+            name='academicFaculty'
+            options={facultyOptions}
+          />
+          <Button htmlType='submit'>Submit</Button>
         </PHForm>
       </Col>
     </Flex>
